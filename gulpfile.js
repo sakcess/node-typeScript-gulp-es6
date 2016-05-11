6966e2ed7ebb3d190807c764a35da03684400b14
sakcess <skris232@gmail.com>
2016-05-11 12:22:40 +1000

    Add comment

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var browserify  = require('browserify');
 var tsify       = require('tsify');
 var source      = require('vinyl-source-stream');
 var buffer      = require('vinyl-buffer');
-//commment 
+//comment
 var bases = {
   app: 'app/',
   dist: 'dist/',
