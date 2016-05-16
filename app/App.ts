bd092e0c64fea64a134f4b795c3cc53d45bf8606
sakcess <skris232@gmail.com>
2016-05-17 09:45:38 +1000

    - blockScopeVariables - constant

--- app/App.ts
+++ app/App.ts
@@ -3,3 +3,5 @@ import AvengersModule = require('./Avengers');
 var superHeros = new AvengersModule.Avengers();
 console.log('IronMan is teamed up with Avengers now.', JSON.stringify(superHeros));
 superHeros.attack();
+superHeros.blockScopeVariables();
+superHeros.constant();
--- app/Avengers.ts
+++ app/Avengers.ts
@@ -2,10 +2,30 @@ import IronManModule = require('./IronMan');
 
 export class Avengers extends IronManModule.IronMan
 {
+
+
   attack()
   {
     super.attack();
     console.log('Captaim America: You and me, we stay here on the ground, keep the fighting here. And Hulk? Smash!');
     console.log('Hulk: Puny God');
+  }
+
+  blockScopeVariables() {
+    if(true) {
+      var phoneModel = "Android";
+    }
+    console.log(phoneModel);
+
+    // for(var i=0;i<4;i++) {
+    //   console.log('in-side loop', i);
+    // }
+    // console.log('out-side loop', i);
+  }
+
+  constant() {
+    const API_KEY = 'dfsdfsdfsdfsdf';
+    //const API_KEY = 'Changed it!';
+    console.log('API_KEY is: ', API_KEY);
   }
 }
